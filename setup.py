72cadda60ba94c0c10bb20a3014a7df1fba7f9f1
liuyug <liuyug@gmail.com>
2016-09-01 15:48:28 +0800

    add pyinstaller and use setuptools

--- setup.py
+++ setup.py
@@ -5,27 +5,10 @@ import os
 import sys
 import subprocess
 
-from distutils.core import setup
-from distutils.command import install_scripts
+from setuptools import setup
 
 from alltray import __version__
 
-
-class post_install_scripts(install_scripts.install_scripts):
-    """ remove script ext """
-    def run(self):
-        install_scripts.install_scripts.run(self)
-        for script in self.get_outputs():
-            if script.endswith(".py"):
-                new_name = script[:-3]
-                if os.path.exists(new_name):
-                    os.remove(new_name)
-                print('renaming %s -> %s' % (script, new_name))
-                os.rename(script, new_name)
-                cmd = ['sed', '-i', '-r', 's/Exec=.*$/Exec=%s/' % new_name.replace('/', '\/'), 'alltray.desktop']
-                print(' '.join(cmd))
-                subprocess.call(cmd)
-        return
 
 data_files = []
 if sys.platform in ['linux', 'linux2']:
@@ -40,13 +23,14 @@ setup(name='AllTray',
       description='Tray all application',
       long_description=open('README.rst').read(),
       platforms=['noarch'],
-      scripts=['alltray.py'],
       packages=[
           'alltray',
       ],
-      requires=['pyqt4'],
       data_files=data_files,
-      cmdclass={
-          'install_scripts': post_install_scripts,
+      entry_points={
+          'console_scripts': [
+              'alltray = alltray.tray:main',
+          ],
       },
+      zip_safe=False,
       )
